4a929f4bcd906caf498ede9ed607090712c09235
Muhammad Annas <ma2259050@gmail.com>
2025-01-23 15:48:26 +0500

    first commit

--- auth.config.ts
+++ auth.config.ts
@@ -4,7 +4,8 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
-  callbacks: {
+
+callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
